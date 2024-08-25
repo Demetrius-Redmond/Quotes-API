9910bab7c8bda036da8c72e184191e112fc4f72c
Demetrius Redmond <demetriusredmondofl@outlook.com>
2024-08-24 22:57:18 -0500

    trimmed message to only the quote

--- main.py
+++ main.py
@@ -10,8 +10,18 @@ def quote_by_category(category: str):
     api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
     response = requests.get(api_url, headers={'X-Api-Key': os.getenv("api_key")})
     if response.status_code == requests.codes.ok:
-        print(response.text)
+        quote = response.text
+        final = ""
+        for x in range(len(quote)):
+            if quote[x] == " " and quote[x+1] == '"':
+                start = x + 1
+                continue
+            if quote[x] == "," and quote[x-1] == '"':
+                end = x
+                break
+        print(quote[start:end])
     else:
         print("Error:", response.status_code, response.text)
 
 quote_by_category("inspirational")
+
