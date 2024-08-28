6a435e60c3336061a54ca275294ead087bbfbb93
Demetrius Redmond <demetriusredmondofl@outlook.com>
2024-08-28 08:19:40 -0500

    js to html test

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+// javascript code from api-ninja
+// require('dotenv').config()
+// console.log(process.env) // remove this after you've confirmed it is working
+
+// var category = "inspirational"
+// $.ajax({
+//     method: 'GET',
+//     url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+//     headers: { 'X-Api-Key': "tzZ8p3IyF/CQ3a+baum6xg==GyuDa8k6FFznvvOu"},
+//     contentType: 'application/json',
+//     success: function(result) {
+//         console.log(result);
+//     },
+//     error: function ajaxError(jqXHR) {
+//         console.error('Error: ', jqXHR.responseText);
+//     }
+// });
+
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
